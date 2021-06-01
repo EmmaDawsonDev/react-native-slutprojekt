8157c43deacee0723ef4cb1a43e5586649917a3f
Renzo Santa Maria <renzosantamaria@Renzos-MacBook-Pro.local>
2021-06-01 15:30:39 +0200

    add styling to logIn page

---
+++ src/constants/color.js
@@ -0,0 +1,24 @@
+export default {
+    primaryDark: '#242529',
+    secondaryDark: '#2e3032',
+    blue: '#748afb',
+    red: '#b4124c',
+    pelleGreen: '#03813d',
+    pink: '#f1c0c0',
+    orange: '#ff9f00',
+    grey: '#dedede',
+    inactive: '#909090'
+}
+
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
+
+// Works only on IOS
+// shadowColor: 'black',
+// shadowOffset:{ width: 0, height:2 },
+// shadowRadius: 6,
+// shadowOpacity: 0.26,
+
+// Works only on Android
+// elevation: 5
--- src/screens/shared/Login.js
+++ src/screens/shared/Login.js
@@ -1,6 +1,10 @@
 import React, { useState, useContext } from "react";
-import { SafeAreaView, Text, TextInput, Button, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from "react-native";
 import AuthContext from "../../store/AuthContext";
+import Color from "../../constants/color"
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+// const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 const LoginScreen = (props) => {
   const { signIn } = useContext(AuthContext);
@@ -12,42 +16,125 @@ const LoginScreen = (props) => {
   const pressHandler = () => {
     signIn({ email, password });
   };
-
+  
   return (
-    <SafeAreaView style={{ flex: 1, padding: 60 }}>
-      <Text>Email</Text>
-      <TextInput
-        style={{
-          borderColor: "black",
-          borderStyle: "solid",
-          borderWidth: 2,
-          backgroundColor: "#dedede",
-          height: 40,
-        }}
-        onChangeText={(text) => {
-          setEmail(text);
-        }}
-        value={email}
-        placeholder="This is the email"
-      />
-      <Text>Password</Text>
-      <TextInput
-        style={{
-          borderColor: "black",
-          borderStyle: "solid",
-          borderWidth: 2,
-          backgroundColor: "#dedede",
-          height: 40,
-        }}
-        onChangeText={(text) => {
-          setPassword(text);
-        }}
-        value={password}
-        placeholder="This is the password"
-      />
-      <Button title="Log in" onPress={pressHandler} />
+    <SafeAreaView style={styles.screen}>
+      <View style={styles.keyWrapper}>
+        <Icon name="key" size={30} color={Color.orange} />
+      </View>
+
+      <View style={styles.loginForm}>
+        <View style={styles.emailInputWrapper}>
+          <Text style={styles.label} >Email</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize='none'
+            keyboardType="email-address"
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+            value={email}
+            placeholder="This is the email"
+            />
+        </View>
+
+        <Text style={styles.label} >Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize='none'
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          value={password}
+          placeholder="This is the password"
+          />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={pressHandler}>
+        <Text style={styles.buttonText} >LOGIN</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  screen:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Color.primaryDark
+  },
+  input:{
+    backgroundColor: "#dedede",
+    height: 50,
+    borderRadius: 6,
+    paddingLeft: 10
+  },
+  loginForm:{
+    backgroundColor: Color.secondaryDark,
+    width: '90%',
+    paddingVertical: 60,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    borderBottomWidth: 10,
+    borderBottomColor: Color.blue,
+
+    // Works only on IOS
+    shadowColor: 'black',
+    shadowOffset:{ width: 0, height:2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+
+    // Works only on Android
+    elevation: 5
+  },
+  label:{
+    color: 'white',
+    marginBottom: 4
+  },
+  button:{
+    height:50,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginTop: 40,
+
+  //   // Works only on IOS
+  //   shadowColor: 'green',
+  //   shadowOffset:{ width: 0, height:2 },
+  //   shadowRadius: 6,
+  //   shadowOpacity: 0.26,
+
+  // // Works only on Android
+  //   elevation: 5
+  },
+  buttonText:{
+    fontSize: 20
+  },
+  emailInputWrapper:{
+    marginBottom: 40
+  },
+  keyWrapper:{
+    backgroundColor: Color.secondaryDark,
+    height:90,
+    width:90,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    marginBottom: -50,
+    zIndex: 2,
+
+    // Works only on IOS
+    shadowColor: 'black',
+    shadowOffset:{ width: 0, height:2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.26,
+
+    // Works only on Android
+    elevation: 10,
+  }
+})
 
 export default LoginScreen;
