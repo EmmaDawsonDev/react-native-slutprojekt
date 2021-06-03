f4fb92b5bbe49fea9264170f18dd38d08a6d3db1
Renzo Santa Maria <renzosantamaria@Renzos-MacBook-Pro.local>
2021-06-03 11:11:38 +0200

    Add filter handler

--- src/components/TaskFilter.js
+++ src/components/TaskFilter.js
@@ -9,16 +9,16 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 const TaskFilter = ({ setTasks }) => {
   const [filter, setFilter] = useState('')
   const [search, setSearch] = useState('')
-  const user = useContext(AuthContext)
+  const {user} = useContext(AuthContext)
 
-  const getFilteredTasks = async () => {
+  const getFilteredTasks = async() => {
     const tasks = await getTasks(user.token, filter, search)
-    setTasks([...tasks])
+    setTasks(tasks.tasks)
   }
 
   return (
     <View style={styles.wrapper}>
-      <TextInput style={styles.searchInput} placeholder="Search name..." />
+      <TextInput onChangeText={ inputText => {setSearch(inputText)}} value={search} style={styles.searchInput} placeholder="Search name..." />
       <RNPickerSelect style={{ inputIOS: styles.searchInput, inputAndroid: styles.searchInput }}
         placeholder={{}}
         onValueChange={(value) => setFilter(value)}
@@ -27,6 +27,9 @@ const TaskFilter = ({ setTasks }) => {
           { label: 'Done', value: 'done' },
           { label: 'Incomplete', value: 'incomplete' },
         ]} />
+        <TouchableOpacity onPress={getFilteredTasks} style={styles.searchButton}>
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -37,14 +40,45 @@ const styles = StyleSheet.create({
     backgroundColor: Color.secondaryDark,
     width: '90%',
     borderLeftColor: Color.blue,
-    borderLeftWidth: 10
+    borderLeftWidth: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    height: 190,
+    display: 'flex',
+    justifyContent: 'space-between',
+    // Border and Shadow ---//
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+    // Works only on IOS
+    shadowColor: 'black',
+    shadowOffset:{ width: 0, height:2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+
+    // Works only on Android
+    elevation: 5
   },
   searchInput: {
     backgroundColor: Color.grey,
-    height: 50,
+    height: 40,
     borderRadius: 6,
     paddingLeft: 10
+  },
+  searchButton: {
+    backgroundColor: Color.blue,
+    width: '30%',
+    paddingVertical: 10,
+    borderRadius: 8,
+    alignSelf: 'flex-end'
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 16,
+    textTransform: 'uppercase'
   }
+    
 })
 
 export default TaskFilter
