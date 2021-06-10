dbf9b748197cf7ac10441e17e981d394f1ab48aa
Emma Dawson <emma.l.dawson@gmail.com>
2021-06-10 12:00:56 +0200

    Add navigate to single user from client singletask

--- src/navigators/ClientTaskStack.js
+++ src/navigators/ClientTaskStack.js
@@ -3,15 +3,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import ClientSingleTask from "../screens/client/ClientSingleTask";
 import ClientTasks from "../screens/client/ClientTasks";
-import Color from '../constants/color'
+import SingleUser from "../screens/shared/SingleUser";
+import Color from "../constants/color";
 
 const Stack = createStackNavigator();
 
 const ClientTaskStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: Color.grey } }} initialRouteName="ClientTasks">
+    <Stack.Navigator
+      screenOptions={{ headerStyle: { backgroundColor: Color.grey } }}
+      initialRouteName="ClientTasks"
+    >
       <Stack.Screen name="ClientTasks" component={ClientTasks} />
       <Stack.Screen name="ClientSingleTask" component={ClientSingleTask} />
+      <Stack.Screen name="SingleUser" component={SingleUser} />
     </Stack.Navigator>
   );
 };
